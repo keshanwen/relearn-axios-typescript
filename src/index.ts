1cc90d1eda90600fae8406afe728796b9e89262a
keshanwen <3640@winnermedical.com>
2022-09-20 10:51:09 +0800

    feat:处理请求url参数

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
+import { bulidURL } from './helpers/url'
 
 function axios(config: AxiosRequestConfig) {
+  // 处理请求配置
+  processConfig(config)
   xhr(config)
 }
+
+
+
+
+
+
+function processConfig(config: AxiosRequestConfig): void {
+  // 处理请求url， 将请求参数拼接到URL上
+  config.url = transformUrl(config)
+}
+
+
+
+function transformUrl(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return bulidURL(url, params)
+}
+
+
 
 export default axios
